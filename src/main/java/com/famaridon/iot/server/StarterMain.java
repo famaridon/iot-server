bf5124b6086907c3e36176a4fba00756a423a9c6
famaridon <famaridon@visiativ.com>
2016-08-24 16:33:00 +0200

    remove dev comments

--- src/main/java/com/famaridon/iot/server/StarterMain.java
+++ src/main/java/com/famaridon/iot/server/StarterMain.java
@@ -1,7 +1,5 @@
 package com.famaridon.iot.server;
 
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
 import org.wildfly.swarm.Swarm;
 import org.wildfly.swarm.cdi.CDIFraction;
 import org.wildfly.swarm.datasources.DatasourcesFraction;
@@ -9,7 +7,6 @@ import org.wildfly.swarm.ejb.EJBFraction;
 import org.wildfly.swarm.jaxrs.JAXRSFraction;
 import org.wildfly.swarm.jpa.JPAFraction;
 import org.wildfly.swarm.logging.LoggingFraction;
-import org.wildfly.swarm.undertow.WARArchive;
 
 /**
  * Created by famaridon on 22/08/2016.
@@ -42,12 +39,6 @@ public class StarterMain
 		swarm.fraction(LoggingFraction.createDefaultLoggingFraction());
 		
 		swarm.start();
-		
-		WARArchive deployment = ShrinkWrap.create(WARArchive.class);
-		deployment.addPackage("com.famaridon.iot.server");
-		deployment.addAsWebInfResource(new ClassLoaderAsset("META-INF/persistence.xml", StarterMain.class.getClassLoader()), "classes/META-INF/persistence.xml");
-		deployment.addAsWebInfResource(new ClassLoaderAsset("META-INF/beans.xml", StarterMain.class.getClassLoader()), "classes/META-INF/beans.xml");
-		deployment.addAllDependencies();
 		
 		swarm.deploy(swarm.createDefaultDeployment());
 		 
