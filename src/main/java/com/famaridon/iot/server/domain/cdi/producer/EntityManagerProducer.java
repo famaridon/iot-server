5fafab8d2b9b5f0a5dbd0afa1158ef301881fce6
famaridon <famaridon@visiativ.com>
2016-08-24 16:05:47 +0200

    Failled to link

---
+++ src/main/java/com/famaridon/iot/server/domain/cdi/producer/EntityManagerProducer.java
@@ -0,0 +1,12 @@
+package com.famaridon.iot.server.domain.cdi.producer;
+
+/**
+ * Created by famaridon on 24/08/2016.
+ */
+public class EntityManagerProducer
+{
+	/**
+	 * the default class logger
+	 */
+	private static final com.axemble.vdoc.sdk.utils.Logger LOG = com.axemble.vdoc.sdk.utils.Logger.getLogger(EntityManagerProducer.class);
+}
