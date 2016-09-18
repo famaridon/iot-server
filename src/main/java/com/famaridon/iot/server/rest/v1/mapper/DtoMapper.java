8703bdb72dad3473ffbe2112562a91dcff99e1ef
famaridon <famaridon@visiativ.com>
2016-09-18 17:02:53 +0200

    configure swagger

--- src/main/java/com/famaridon/iot/server/rest/v1/mapper/DtoMapper.java
+++ src/main/java/com/famaridon/iot/server/rest/v1/mapper/DtoMapper.java
@@ -1,7 +1,11 @@
 package com.famaridon.iot.server.rest.v1.mapper;
 
+import com.famaridon.iot.server.domain.entities.Device;
 import com.famaridon.iot.server.domain.entities.User;
+import com.famaridon.iot.server.domain.entities.Value;
+import com.famaridon.iot.server.rest.v1.dto.DeviceDto;
 import com.famaridon.iot.server.rest.v1.dto.UserDto;
+import com.famaridon.iot.server.rest.v1.dto.ValueDto;
 import org.mapstruct.Mapper;
 
 /**
@@ -11,5 +15,9 @@ import org.mapstruct.Mapper;
 public interface DtoMapper
 {
 	UserDto userToUserDto(User user);
+	
+	DeviceDto deviceToDeviceDto(Device device);
+	
+	ValueDto valueToValueDto(Value value);
 	
 }
